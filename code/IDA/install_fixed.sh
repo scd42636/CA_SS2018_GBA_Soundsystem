bd3eebc1aea7cfc22627e293ad5a8bd61b931337
Dominik Scharnagl <dominik1.scharnagl@st.oth-regensburg.de>
2018-05-30 22:54:45 +0200

    Imported from Google Drive.

---
+++ code/IDA/install_fixed.sh
@@ -0,0 +1,11 @@
+sudo apt-get install lsb-core
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install lib32gcc1 libc6-i686:i386 libstdc++6:i386
+chmod 777 idaq
+chmod 777 idaq64
+sudo apt-get install libglib2.0-0:i386
+sudo apt-get install libgtk2.0.0:i386 libidn11:i386 libglu1-mesa:i386
+sudo apt-get install libsm6:i386
+
+
